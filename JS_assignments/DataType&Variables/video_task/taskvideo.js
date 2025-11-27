33b2341a25ed8a775d8f8e5203cab7fbd7b19162
ezzaytouni othmane <ezzaytouniothmane@gmail.com>
2025-11-27 13:44:46 +0100

    javascript assignments

---
+++ JS_assignments/DataType&Variables/video_task/taskvideo.js
@@ -0,0 +1,9 @@
+let title = "Elzero";
+let desc = 'Elzero Web School';
+let date = "20/10";
+let d = `
+    <h3>Hello ${title}</h3>
+    <p>${desc}</p>
+    <span>${date}</span>
+`;
+document.write(d.repeat(4));
---
+++ JS_assignments/Introduction/task2/task2.js
@@ -0,0 +1,5 @@
+document.getElementById("h1").style.color = "blue";
+document.getElementById("h1").style.fontSize = "80px";
+document.getElementById("h1").style.fontFamily = "arial";
+document.getElementById("h1").style.textAlign = "center";
+document.getElementById("h1").style.fontWeight = "bold";
