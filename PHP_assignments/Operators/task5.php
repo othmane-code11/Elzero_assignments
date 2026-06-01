39c8867099518c4c13f2703e47eebce99550c0ab
othmane-code11 <134103443+othmane-code11@users.noreply.github.com>
2026-06-01 02:20:39 +0200

    Refactor task5.php for point calculations

--- PHP_assignments/Operators/task5.php
+++ PHP_assignments/Operators/task5.php
@@ -1,10 +1,10 @@
-<?php
+<?php 
     $points = 10;
-    $t = true;
+    $t = +true;
     // Write Your Code Here
-    $points += $t + $t + $t;
-    echo $points; // 13
-    
+    $points += $points / ++$t - $t;
+    echo $points . "<br>"; // 13
+
     // Write Your Code Here
-    $points = $points - $t - $t - $t - $t - $t;
+    $points -= ++$points / $t - --$t;
     echo $points; // 8;
